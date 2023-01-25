5940aac55b1061eee7700f21ce6c7b682d78d0b2
John <john@jbrereton.com>
2023-01-24 19:23:49 -0500

    Update and clean hardware class

--- RRBotHardware.java
+++ RRBotHardware.java
@@ -28,32 +28,19 @@ public class RRBotHardware
     public Servo frontRightTurn = null;
     public Servo rearLeftTurn = null;
     public Servo rearRightTurn = null;
-    public AnalogInput frontLeftEnc = null;
-    public AnalogInput frontRightEnc = null;
-    public AnalogInput rearLeftEnc = null;
-    public AnalogInput rearRightEnc = null;
     public DcMotor armMotor = null;
     public Servo clawServo = null;
-
-    public enum SERVOS {
-        FRONT_LEFT,
-        FRONT_RIGHT,
-        REAR_LEFT,
-        REAR_RIGHT
-    }
-
-    /** Set the multiplier for encoders */
-    // Current value is for 3.3v Encoder, Lamprey2 Encoder Specifically
-    public final double ENCODER_TO_ANGLE = 155; // 72.0 for 5.0v
+/*    public AnalogInput frontLeftEnc = null;
+    public AnalogInput frontRightEnc = null;
+    public AnalogInput rearLeftEnc = null;
+    public AnalogInput rearRightEnc = null;*/
 
     /* local OpMode members. */
     HardwareMap hwMap = null;
     private ElapsedTime period = new ElapsedTime();
 
     /* Constructor */
-    public RRBotHardware(){
-
-    }
+    public RRBotHardware(){}
 
     /* Initialize standard Hardware interfaces */
     public void init(HardwareMap ahwMap) {
@@ -65,62 +52,61 @@ public class RRBotHardware
         frontRightDrive = hwMap.get(DcMotor.class, "front_right_drive");
         rearLeftDrive = hwMap.get(DcMotor.class, "rear_left_drive");
         rearRightDrive = hwMap.get(DcMotor.class, "rear_right_drive");
+        armMotor = hwMap.get(DcMotor.class, "arm_motor");
 
-        frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
-        frontRightDrive.setDirection(DcMotor.Direction.REVERSE);
+        frontLeftDrive.setDirection(DcMotor.Direction.FORWARD);
+        frontRightDrive.setDirection(DcMotor.Direction.FORWARD);
         rearLeftDrive.setDirection(DcMotor.Direction.FORWARD);
         rearRightDrive.setDirection(DcMotor.Direction.FORWARD);
+        armMotor.setDirection(DcMotor.Direction.FORWARD);
 
         // Set motor power on init
         frontLeftDrive.setPower(0);
         frontRightDrive.setPower(0);
         rearLeftDrive.setPower(0);
         rearRightDrive.setPower(0);
+        armMotor.setPower(0);
 
         // Set motor run modes
         frontLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         frontRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rearLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rearRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        armMotor.setTargetPosition(0);
+        armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
         // Set motor zero power behavior
         frontLeftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         frontRightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rearLeftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rearRightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
 
         //Define and Initialize Servos
         frontLeftTurn = hwMap.get(Servo.class, "front_left_turn");
         frontRightTurn = hwMap.get(Servo.class, "front_right_turn");
         rearLeftTurn = hwMap.get(Servo.class, "rear_left_turn");
         rearRightTurn = hwMap.get(Servo.class, "rear_right_turn");
+        clawServo = hwMap.get(Servo.class, "claw_servo");
 
         frontLeftTurn.setDirection(Servo.Direction.REVERSE);
         frontRightTurn.setDirection(Servo.Direction.REVERSE);
         rearRightTurn.setDirection(Servo.Direction.REVERSE);
         rearLeftTurn.setDirection(Servo.Direction.REVERSE);
+        clawServo.setDirection(Servo.Direction.FORWARD);
 
         // Set servo power on init
-        frontLeftTurn.setPosition(0);
-        frontRightTurn.setPosition(0);
-        rearLeftTurn.setPosition(0);
-        rearRightTurn.setPosition(0);
-
-        frontLeftEnc = hwMap.get(AnalogInput.class, "front_left_enc");
-        frontRightEnc = hwMap.get(AnalogInput.class, "front_right_enc");
-        rearLeftEnc = hwMap.get(AnalogInput.class, "rear_left_enc");
-        rearRightEnc = hwMap.get(AnalogInput.class, "rear_right_enc");
-
-        armMotor = hwMap.get(DcMotor.class, "arm_motor");
-        armMotor.setDirection(DcMotor.Direction.FORWARD);
-        armMotor.setPower(0);
-        armMotor.setTargetPosition(0);
-        armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        clawServo = hwMap.get(Servo.class, "claw_servo");
-        clawServo.setDirection(Servo.Direction.FORWARD);
+        frontLeftTurn.setPosition(0.5);
+        frontRightTurn.setPosition(0.5);
+        rearLeftTurn.setPosition(0.5);
+        rearRightTurn.setPosition(0.5);
         clawServo.setPosition(1);
 
+        /*frontLeftEnc = hwMap.get(AnalogInput.class, "front_left_enc");
+        frontRightEnc = hwMap.get(AnalogInput.class, "front_right_enc");
+        rearLeftEnc = hwMap.get(AnalogInput.class, "rear_left_enc");
+        rearRightEnc = hwMap.get(AnalogInput.class, "rear_right_enc");*/
     }
 }
